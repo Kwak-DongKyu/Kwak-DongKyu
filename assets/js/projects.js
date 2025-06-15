8a47d890b942a95c55b25eb4137d3944804613f8
Kwak-DongKyu <91858707+Kwak-DongKyu@users.noreply.github.com>
2025-06-15 15:08:29 +0900

    Update projects.js

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -20,7 +20,7 @@ let render_projects = (slug) => {
             categories: ['featured', 'webdev']
         },
         {
-            image: 'assets/images/wrimoucon_main.png',
+            image: null,
             link: 'https://ieeexplore.ieee.org/abstract/document/10224423',
             title: 'WriMouCon: Wrist-Mounted Haptic Controller for Rendering Physical Properties in Virtual Reality',
             demo: false,
