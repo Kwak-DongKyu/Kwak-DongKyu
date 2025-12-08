d6cd775699a9b4663bb5e1494fafb57d50c70ff5
DongKyu Kwak <pos07159@naver.com>
2025-12-09 08:22:01 +0900

    Updates

--- assets/ModelViewer-B26L4MFm.js
+++ assets/ModelViewer-B26L4MFm.js
@@ -1,4 +1,4 @@
-import{r as Me,g as cA,j as Dt,c as uA,R as hA}from"./index-BZx77-zI.js";/**
+import{r as Me,g as cA,j as Dt,c as uA,R as hA}from"./index-oO3xDz66.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
