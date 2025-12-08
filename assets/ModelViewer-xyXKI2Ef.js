11548b7a71840548b54b718c069b3a3c2ea866e3
DongKyu Kwak <pos07159@naver.com>
2025-12-09 08:48:07 +0900

    Updates

--- assets/ModelViewer-xyXKI2Ef.js
+++ assets/ModelViewer-xyXKI2Ef.js
@@ -1,4 +1,4 @@
-import{r as Me,g as cA,j as Dt,c as uA,R as hA}from"./index-oO3xDz66.js";/**
+import{r as Me,g as cA,j as Dt,c as uA,R as hA}from"./index-D886vTzn.js";/**
  * @license
  * Copyright 2010-2023 Three.js Authors
  * SPDX-License-Identifier: MIT
